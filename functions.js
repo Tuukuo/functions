208ee4445a6a32afaaa42d041cb2ce087723a397
Tuukuo <tuukuodorcas@gmail.com>
2024-03-29 19:52:52 +0300

    add files

---
+++ functions.js
@@ -0,0 +1,13 @@
+//use a function to reverse all elements in the string in ascending order and the specific elements in the descending order
+function arrange(){
+    let names=["John","Mary","Emily","Naomy"]
+    console.log(names.sort())
+    console.log(names.reverse)
+}
+
+function employees(workers){
+    const a=workers
+    const newArray=[...newSet(a)]
+    return(newArray.sort())
+}
+console.log("40010712","Dorcas","20000")
